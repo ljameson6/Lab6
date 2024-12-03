bc153e18b454521bdf85b57276aca58d6524fba3
ljameson6 <ljameson6@gatech.edu>
2024-12-03 15:07:42 -0500

    added -2

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 
 double xSquared(int samples, int taskid);
+double e(int samples, int taskid);
 
 #define MASTER 0        /* task ID of master task */
 
@@ -15,7 +16,6 @@ int main(int argc, char* argv[])
 {
     MPI_Status status;
     MPI_Init(&argc, &argv);
-    std::cout << "1" << std::endl;
 
     //Parse input args
     std::map<std::string, std::string> mp;
@@ -50,10 +50,19 @@ int main(int argc, char* argv[])
     std::cout << "MPI task " << taskid << " has started..." << std::endl;
 
     if (taskid == MASTER) {
-        homesum = xSquared(samplesPerTask + (N - samplesPerTask * numtasks), taskid);
+        if (P == 2) {
+            homesum = e(samplesPerTask + (N - samplesPerTask * numtasks), taskid);
+        } else {
+            homesum = xSquared(samplesPerTask + (N - samplesPerTask * numtasks), taskid);
+        }
     }
     else {
-        homesum = xSquared(samplesPerTask, taskid);
+        if (P == 2) {
+            homesum = e(samplesPerTask, taskid);
+        }
+        else {
+            homesum = xSquared(samplesPerTask, taskid);
+        }
     }
 
     rc = MPI_Reduce(&homesum, &totsum, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
@@ -74,16 +83,31 @@ double xSquared(int samples, int taskid)
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
     double sum, x;
-    int score, n;
-    double intpart;
 
     sum = 0;
     /* "throw darts at board" */
-    for (n = 0; n < samples; n++) {
+    for (int n = 0; n < samples; n++) {
         /* generate random numbers for x and y coordinates */
         x = dis(gen);
-        x = modf(x, &intpart);
         sum += x * x;
+    }
+    return(sum);
+}
+
+double e(int samples, int taskid)
+{
+    std::default_random_engine gen;
+    gen.seed(taskid);
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+
+    double sum, x;
+
+    sum = 0;
+    /* "throw darts at board" */
+    for (int n = 0; n < samples; n++) {
+        /* generate random numbers for x and y coordinates */
+        x = dis(gen);
+        sum += exp(-1 * x * x);
     }
     return(sum);
 }
